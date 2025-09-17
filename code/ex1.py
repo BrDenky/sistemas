e6b35efbe062515ded03d4199cb6475c251b8991
BrDenky <mateopilaquinga@gmail.com>
2025-09-17 14:39:06 -0500

    dsds

--- code/ex1.py
+++ code/ex1.py
@@ -1,8 +1,19 @@
 import os
 import time
 
+# Primer fork
 pid1 = os.fork()
+
+# Segundo fork
 pid2 = os.fork()
+
+# Tercer fork
 pid3 = os.fork()
 
-time.sleep(20)
+# Identificador único de cada proceso según los valores de retorno
+print(
+    f"Proceso: PID={os.getpid()}, PPID={os.getppid()}, "
+    f"pid1={pid1}, pid2={pid2}, pid3={pid3}"
+)
+
+time.sleep(20)  # mantener procesos vivos para inspección
